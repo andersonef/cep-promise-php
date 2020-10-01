8f662d784d46a6743223542b63245646b0c0fd73
Claudson Oliveira <claudsonweb@gmail.com>
2020-10-01 18:13:10 +0100

    Make sure that we just hit one service

--- tests/CepPromiseTest.php
+++ tests/CepPromiseTest.php
@@ -4,6 +4,7 @@ namespace Andersonef\CepPromise\Tests;
 
 use Andersonef\CepPromise\CepPromise;
 use Andersonef\CepPromise\Exceptions\CepPromiseException;
+use Andersonef\CepPromise\Interfaces\CepServiceInterface;
 use Andersonef\CepPromise\ResponseAddress;
 use PHPUnit\Framework\TestCase;
 
@@ -23,5 +24,32 @@ final class CepPromiseTest extends TestCase
         $cep = '00000000';
         $this->expectException(CepPromiseException::class);
         $response = (new CepPromise($cep))->fetch()->wait();
+    }
+
+    public function testShouldThrowExceptionIfThereIsNoService()
+    {
+        $cep = '83322-170';
+        $this->expectException(CepPromiseException::class);
+        $promiser = new CepPromise($cep);
+        $promiser->clearServices();
+        $response = $promiser->fetch()->wait();
+    }
+
+    public function testShouldAvoidFetchingInAllTheServices()
+    {
+        $cep = '83322-170';
+        $service1 = $this->createMock(CepServiceInterface::class);
+        $service2 = $this->createMock(CepServiceInterface::class);
+        $response = $this->createMock(ResponseAddress::class);
+
+
+        $service1->expects($this->once())->method('fetch')->willReturn($response);
+        $service2->expects($this->never())->method('fetch')->willReturn($response);
+
+        $promise = new CepPromise($cep, null, null, $service1);
+        $promise->appendCepService($service2);
+
+
+        $response = $promise->fetch()->wait();
     }
 }
